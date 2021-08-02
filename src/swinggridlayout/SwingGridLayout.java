52a061f7f1fd332db6861a069032d40cc01c0a1f
Ankur Kumar Aggarwal <40783014+ankur-agg1987@users.noreply.github.com>
2021-08-02 10:33:08 +0530

    initial commit

---
+++ src/swinggridlayout/SwingGridLayout.java
@@ -0,0 +1,29 @@
+package swinggridlayout;
+
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class SwingGridLayout {
+
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        
+        f.setLayout(new GridLayout(4, 2));
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    f.add(new JButton("Button One"));
+    f.add(new JButton("Button Two"));
+    f.add(new JButton("Button Three"));
+    f.add(new JButton("Button Four"));
+    f.add(new JButton("Button Five"));
+    f.add(new JButton("Button Six"));
+    f.add(new JLabel("Hello"));
+    f.add(new JButton("Button Seven"));
+    
+    
+    f.setSize(400, 400);
+    f.setVisible(true);
+    }
+    
+}

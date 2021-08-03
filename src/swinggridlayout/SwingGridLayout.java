5aaf1bac5351689bbe4de4e504ccca067967bc29
Ankur Kumar Aggarwal <40783014+ankur-agg1987@users.noreply.github.com>
2021-08-03 14:30:25 +0530

    change of component flow of addition in container

--- src/swinggridlayout/SwingGridLayout.java
+++ src/swinggridlayout/SwingGridLayout.java
@@ -8,15 +8,20 @@ import javax.swing.JLabel;
 public class SwingGridLayout {
 
     public static void main(String[] args) {
-        JFrame f = new JFrame();
+        JFrame f = new JFrame("Swing Grid Layout Demo");
         
         f.setLayout(new GridLayout(4, 2));
+        
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    f.add(new JButton("Button One"));
-    f.add(new JButton("Button Two"));
-    f.add(new JButton("Button Three"));
+        
+        // we need to consider the flow of adding the components in container with gridlayout
+         f.add(new JButton("Button Three"));
     f.add(new JButton("Button Four"));
-    f.add(new JButton("Button Five"));
+    f.add(new JButton("Button One"));
+     f.add(new JButton("Button Five"));
+    f.add(new JButton("Button Two"));
+   
+   ;
     f.add(new JButton("Button Six"));
     f.add(new JLabel("Hello"));
     f.add(new JButton("Button Seven"));
